c4707ca4efc6a5c2eaeb59adae574e0e59a1edf9
tdvaughan <tim@timvaughan.co.uk>
2023-02-23 14:27:16 +0000

    Update spear.py

--- spear.py
+++ spear.py
@@ -2,36 +2,8 @@ import plasma
 from plasma import plasma_stick
 import time
 from random import random, uniform
-import colorsys
-import ioexpander as io
-
-I2C_ADDR = 0x0F  # 0x18 for IO Expander, 0x0F for the encoder breakout
-
-PIN_RED = 1
-PIN_GREEN = 7
-PIN_BLUE = 2
-
-POT_ENC_A = 12
-POT_ENC_B = 3
-POT_ENC_C = 11
-
-BRIGHTNESS = 0.5                # Effectively the maximum fraction of the period that the LED will be on
-PERIOD = int(255 / BRIGHTNESS)  # Add a period large enough to get 0-255 steps at the desired brightness
-
-ioe = io.IOE(i2c_addr=I2C_ADDR, interrupt_pin=4)
-
-# Swap the interrupt pin for the Rotary Encoder breakout
-if I2C_ADDR == 0x0F:
-    ioe.enable_interrupt_out(pin_swap=True)
-
-ioe.setup_rotary_encoder(1, POT_ENC_A, POT_ENC_B, pin_c=POT_ENC_C)
-
-ioe.set_pwm_period(PERIOD)
-ioe.set_pwm_control(divider=2)  # PWM as fast as we can to avoid LED flicker
-
-ioe.set_mode(PIN_RED, io.PWM, invert=True)
-ioe.set_mode(PIN_GREEN, io.PWM, invert=True)
-ioe.set_mode(PIN_BLUE, io.PWM, invert=True)
+from pimoroni_i2c import PimoroniI2C
+from breakout_encoder import BreakoutEncoder
 
 """
 A basic fire effect.
